d5551997e66a3c2c153f3aa2e4dd8aa712aa189d
Cecilia Sousa <ceciliasousa21@gmail.com>
2019-10-19 11:52:47 -0300

    initial commit

---
+++ app/src/main/java/com/example/agendatelefonicaapp/view/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.agendatelefonicaapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.agendatelefonicaapp.R;
+
+import java.util.Timer;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
